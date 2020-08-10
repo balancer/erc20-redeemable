087eebc49a28f114fd36e4d9fb0923737732dad1
Greg Taschuk <gregorytaschuk@gmail.com>
2020-08-10 11:45:58 -0700

    rework disburse script to match results of balance mining script

--- merkle/scripts/disburse.js
+++ merkle/scripts/disburse.js
@@ -1,4 +1,4 @@
-const Redeem = artifacts.require("./MerkleRedeem.sol");
+//const Redeem = artifacts.require("./MerkleRedeem.sol");
 const { MerkleTree } = require("../lib/merkleTree");
 const { utils, eth } = web3;
 const fs = require("fs");
@@ -7,24 +7,22 @@ const readline = require("readline");
 module.exports = async function(callback) {
   let elements = [];
 
-  let reedem = await Redeem.deployed();
+  //let redeem = await Redeem.deployed();
   console.log("File Path Arg (must be absolute):", process.argv[4]);
 
-  const lineReader = readline.createInterface({
-    input: fs.createReadStream(process.argv[4])
-  });
+  const rawdata = fs.readFileSync(process.argv[4]);
+  const balances = JSON.parse(rawdata);
 
-  lineReader.on("line", line => {
-    let address, balance;
-    [address, balance] = line.split(",");
+  console.log(balances);
+
+  Object.keys(balances).forEach(address => {
+    let balance = balances[address];
     let leaf = utils.soliditySha3(address, parseInt(balance));
     console.log("Adding leaf:\t", leaf, " for ", address, balance);
     elements.push(leaf);
   });
 
-  lineReader.on("close", () => {
-    const merkleTree = new MerkleTree(elements);
-    const root = merkleTree.getHexRoot();
-    console.log("tree has root:\t", root);
-  });
+  const merkleTree = new MerkleTree(elements);
+  const root = merkleTree.getHexRoot();
+  console.log("tree has root:\t", root);
 };
