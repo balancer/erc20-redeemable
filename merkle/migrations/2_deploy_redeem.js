bb84009386c6dc8634dd0a9e1aed6134d28dc4e6
Hammad Jutt <jutt@ualberta.ca>
2021-03-17 23:09:25 -0600

    Finalize Mainnet Deployment

--- merkle/migrations/2_deploy_redeem.js
+++ merkle/migrations/2_deploy_redeem.js
@@ -1,8 +1,5 @@
 const TToken = artifacts.require("./TToken.sol");
 const Redeem = artifacts.require("./MerkleRedeem.sol");
-const { utils } = web3;
-
-const MAX = utils.toTwosComplement(-1);
 
 module.exports = (deployer, network, accounts) => {
   const admin = "0x8F942ECED007bD3976927B7958B50Df126FEeCb5";
@@ -12,10 +9,10 @@ module.exports = (deployer, network, accounts) => {
     // await deployer.deploy(TToken, "Test Bal", "TBAL", 18);
     // const token = await TToken.deployed();
     // await token.mint(admin, utils.toWei("145000"));
-    console.log("deployer");
-    // throw new Error('CANT FINISH!')
 
     await deployer.deploy(Redeem, "0x30cF203b48edaA42c3B4918E955fED26Cd012A3F");
     // await Redeem.deployed();
+    // await token.approve(redeem.address, MAX);
+    // await token.transfer(redeem.address, utils.toWei("20000"));
   });
 };
--- merkle/migrations/3_finish-disburse.js
+++ merkle/migrations/3_finish-disburse.js
@@ -7,15 +7,18 @@ module.exports = (deployer, network, accounts) => {
   const admin = "0x8F942ECED007bD3976927B7958B50Df126FEeCb5";
   deployer.then(async () => {
     console.log("Finish Disburse");
-    throw new Error("CANT FINISH!");
 
     // const token = await TToken.deployed();
     // await token.mint(admin, utils.toWei("5000"));
     //
-    // const redeem = await Redeem.deployed();
+    const redeem = await Redeem.deployed();
+
+    let weekNum = 1; // adjust accordingly
     //
-    // let weekNum = 1 // adjust accordingly
-    //
-    // await redeem.seedAllocations(weekNum, "0xecd3aa0b1483b0a141e7df656c4b8ffd0cf3ace2bd8c9c60b85669f625dedccb", utils.toWei("4450.862343625"));
+    await redeem.seedAllocations(
+      weekNum,
+      "0xecd3aa0b1483b0a141e7df656c4b8ffd0cf3ace2bd8c9c60b85669f625dedccb",
+      utils.toWei("4450.862343625")
+    );
   });
 };
