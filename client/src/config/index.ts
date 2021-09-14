d11ad417a8092f9a79ebe3160caf8caa2befb853
Greg Taschuk <gregorytaschuk@gmail.com>
2021-09-13 21:34:48 -0700

    added config for arbitrum (BAL)

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,9 +1,10 @@
 import homestead from '@/config/homestead.json';
 import homesteadLido from '@/config/homestead-lido.json';
+import arbitrum from '@/config/arbitrum.json';
 import kovan from '@/config/kovan.json';
 
 const configs = {
-  production: { homestead, kovan, homesteadLido }
+  production: { homestead, kovan, homesteadLido, arbitrum }
 };
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
