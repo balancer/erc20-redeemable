c71b78b13d93e091b6fca358c785a1a06c685752
Greg Taschuk <gregorytaschuk@gmail.com>
2021-09-20 10:09:28 -0700

    added vita config

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,10 +1,11 @@
 import homestead from '@/config/homestead.json';
 import homesteadLido from '@/config/homestead-lido.json';
+import homesteadVita from '@/config/homestead-vita.json';
 import arbitrum from '@/config/arbitrum.json';
 import kovan from '@/config/kovan.json';
 
 const configs = {
-  production: { homestead, kovan, homesteadLido, arbitrum }
+  production: { homestead, kovan, homesteadLido, homesteadVita, arbitrum }
 };
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
