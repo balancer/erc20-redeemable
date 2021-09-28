a108260fb0799416b452392c7191f274a1cfb652
Greg Taschuk <gregorytaschuk@gmail.com>
2021-09-28 11:50:55 -0700

    added config for arbitrum mcdex client

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -2,10 +2,11 @@ import homestead from '@/config/homestead.json';
 import homesteadLido from '@/config/homestead-lido.json';
 import homesteadVita from '@/config/homestead-vita.json';
 import arbitrum from '@/config/arbitrum.json';
+import arbitrumMcdex from '@/config/arbitrum-mcdex.json';
 import kovan from '@/config/kovan.json';
 
 const configs = {
-  production: { homestead, kovan, homesteadLido, homesteadVita, arbitrum }
+  production: { homestead, kovan, homesteadLido, homesteadVita, arbitrum, arbitrumMcdex }
 };
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
