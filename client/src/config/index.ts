f0c6944634d6ec4af3bdbd63a103b941448d7288
Greg Taschuk <gregorytaschuk@gmail.com>
2021-10-05 10:13:06 -0700

    added config for pickle distributions on arbitrum

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -3,10 +3,11 @@ import homesteadLido from '@/config/homestead-lido.json';
 import homesteadVita from '@/config/homestead-vita.json';
 import arbitrum from '@/config/arbitrum.json';
 import arbitrumMcdex from '@/config/arbitrum-mcdex.json';
+import arbitrumPickle from '@/config/arbitrum-pickle.json';
 import kovan from '@/config/kovan.json';
 
 const configs = {
-  production: { homestead, kovan, homesteadLido, homesteadVita, arbitrum, arbitrumMcdex }
+  production: { homestead, kovan, homesteadLido, homesteadVita, arbitrum, arbitrumMcdex, arbitrumPickle }
 };
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
